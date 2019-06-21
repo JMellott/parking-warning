b93d31a100adb53f1b27dd0453664975dcf0da41
JMellott <mellott.joshua@gmail.com>
2019-06-20 22:20:49 -0400

    Basic UI skeleton in place

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Search from "./components/search.component";
+import AddWarning from "./components/add-warning.component";
+import ViewWarnings from "./components/view-warnings.component";
+
+import logo from "./logo.svg";
+
+class App extends Component {
+	render() {
+		return (
+    		<Router>
+    			<div className="container">
+      				<nav className="navbar navbar-expand-lg navbar-light bg-light">
+      	  				<a class="navbar-brand" href="https://google.com" target="_blank">
+      	  					<img src={logo} width="30" height="30" alt="google.com"/>
+      	  				</a>
+      	  				<Link to="/" className="navbar-brand">Parking Warnings</Link>
+      	  				<div className="collapse navbar-collapse">
+      	  					<ul className="navbar-nav mr-auto">
+      	  						<li className="navbar-item">
+      	  							<Link to="/" className="nav-link">Zones</Link>
+      	  						</li>
+      	  					</ul>
+      	  				</div>
+      	  			</nav>
+      	  			<br/>
+
+			        <Route path="/" exact component={Search} />
+			        <Route path="/add" component={AddWarning} />
+			        <Route path="/view/:zone" component={ViewWarnings} />
+			    </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
